0487d47b5c4fb0a20ccbbca5881d26793ce67438
Marcos Cardoso <mrplcardoso@gmail.com>
2025-03-19 10:32:26 -0300

    Create hellow-world.py

---
+++ hellow-world.py
@@ -0,0 +1,14 @@
+# "Hello World" é um jargão da programação que indica o código mais simples possível
+# a ser escrito em uma linguagem
+
+# "print()" é uma função do Python que serve para exibir coisas no console.
+# O que deve ser exibido é o conteúdo que fica entro de seus parênteses
+print("Hello World")
+
+name = "Maria"
+print(name)
+
+value = 25
+print(value)
+
+print(value * 150)
